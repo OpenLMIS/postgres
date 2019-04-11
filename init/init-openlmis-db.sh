28c0111fa71114763633b8760c795eef126a105f
Chongsun Ahn <cahn922@yahoo.com>
2019-04-11 12:36:32 -0700

    Add drop extension command for uuid extension
    uuid-ossp extension does not seem to automatically be active all the time. Drop it before creating it to ensure it is active.

--- init/init-openlmis-db.sh
+++ init/init-openlmis-db.sh
@@ -4,4 +4,4 @@ set -e
 
 createdb --username "$POSTGRES_USER" open_lmis
 psql -d open_lmis -c "CREATE EXTENSION postgis;"
-psql -d open_lmis -c 'CREATE EXTENSION "uuid-ossp";'
+psql -d open_lmis -c 'DROP EXTENSION IF EXISTS "uuid-ossp"; CREATE EXTENSION "uuid-ossp";'
